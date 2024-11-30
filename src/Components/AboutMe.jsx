c6907ed15b518900e767cd5b8943ab40eaa9e401
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-11-30 10:58:03 -0500

    updated about me

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,6 +1,6 @@
 const AboutMe = () => {
     return (
-      <section id="about" className="bg-[#243d27] text-white py-16 px-8">
+      <section id="about" className="py-16 text-dark-green px-white8 bg-">
         <div className="max-w-5xl mx-auto space-y-4 text-center">
           <h2 className="text-3xl font-bold">About Me</h2>
           <p className="text-lg">
