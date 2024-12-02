c1877dc9da051f131bdd7f5102f2dd7a0ac3c3f0
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-12-02 10:25:25 -0500

    about me section

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     <section id="about" className="py-16 px-8">
       <div className="text-center">
         <h2 className="text-3xl font-bold text-[#243d27]">About Me</h2>
-        <p className="text-[#243d27]">Hi, I’m Yeabsira! I’m a web developer who loves bringing creative ideas to life. I specialize in React.js, HTML/CSS, JavaScript, Node.js, and TypeScript. My fascination with building and improving things started in college, where I had a blast working on Java projects and figuring out how to make them better.
+        <p className="text-[#243d27]">Hi, I’m ! I’m a web developer who loves bringing creative ideas to life. I specialize in React.js, HTML/CSS, JavaScript, Node.js, and TypeScript. My fascination with building and improving things started in college, where I had a blast working on Java projects and figuring out how to make them better.
 At my current job, I’ve been promoted  of  much I value building trust with clients and delivering work that truly makes an impact. Outside of coding, I’m a big football fan (weekends are for games!) and an avid reader of thriller novels. Oh, and I also enjoy tinkering with fun side projects when inspiration strikes.
 Let’s connect on LinkedIn or GitHub—whether to chat, collaborate, or maybe even work together. Who knows? I could be the person you’ve been looking for!
 </p>
