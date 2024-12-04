5784cd6dbe1b008cf00915565c0b611d4777f900
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-12-04 12:09:14 -0500

    Project order

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -33,7 +33,7 @@ const Projects = () => {
     
     { id: 2, title: "Shop Here", embedLink: "https://shop-ease-sigma-jade.vercel.app/ " },
     { id: 3, title: "Tourist Trail", embedLink: "https://tourist-trail.vercel.app/" },
-    { id: 4, title: "Project Manager", embedLink: "https://project-managment-ten.vercel.app/" },
+    { id: 4, title: "Task Manager", embedLink: "https://project-managment-ten.vercel.app/" },
     { id: 1, title: "Home Of Games", embedLink: "https://homeofgames.vercel.app/" },
   ];
 
