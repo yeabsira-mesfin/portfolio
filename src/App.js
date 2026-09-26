255a5eb50a924564908e1ab793f436de6600e466
Yeabsira Mesfin <72326656+yeabsira-mesfin@users.noreply.github.com>
2026-09-25 22:32:46 -0400

    feat: replace scrolling portfolio with career quest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import './App.css';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import AboutMe from './Components/AboutMe';
-import Projects from './Components/Projects';
-import Skills from './Components/Skills';
-import Updates from './Components/Updates';
-import Contact from './Components/Contact';
-import PortfolioBot from './Components/PortfolioBot';
-import ScrollProgressCircle from './Components/ScrollProgressCircle';
+import CareerQuest from './Components/CareerQuest';
 
 function App() {
-  return (
-    <div className="portfolio-shell">
-      <Navbar />
-      <ScrollProgressCircle />
-      <main>
-        <Hero />
-        <AboutMe />
-        <Projects />
-        <Skills />
-        <Updates />
-        <Contact />
-      </main>
-      <PortfolioBot />
-    </div>
-  );
+  return <CareerQuest />;
 }
 
 export default App;
